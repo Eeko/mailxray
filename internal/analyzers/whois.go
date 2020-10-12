15e4cb55cbce43ae6631856c76796c61e57027f2
Eeko <eeko@iki.fi>
2020-10-12 18:25:37 +0100

    fixed recursive domain search

--- internal/analyzers/whois.go
+++ internal/analyzers/whois.go
@@ -7,7 +7,6 @@ import (
 	"net"
 	"strconv"
 	"strings"
-	//"fmt"
 	"regexp"
 )
 
@@ -15,6 +14,11 @@ const (
 	StopCharacter = "\r\n"
 	WhoisPort     = 43
 )
+
+// for keeping track of what servers have been queried so far.
+var QueriedServers = make(map[string]struct{})
+
+
 
 // performs a whois query as described in RFC3912 and returns the lines as an array
 func Whois(domain string) []string {
@@ -41,7 +45,10 @@ func WhoisQuery(domain, whoisserver string) []string {
 	var lines []string
 	io.Copy(&buf, conn)
 	lines = strings.Split(buf.String(), "\n")
+	QueriedServers[whois_server] = struct{}{} // append the server we just queried to the list
+
 	lines = ReadWhoisResponse(domain, lines) // recursion!
+
 	return lines
 }
 
@@ -50,16 +57,22 @@ func ReadWhoisResponse(domain string, lines []string) []string {
 	for i := 0; i < len(lines); i++ {
 		// TODO: add more ways to recurse as refer is not standard and most whois servers use something else.
 		// BSD WHOIS(1) uses #define WHOIS_SERVER_ID	"Whois Server: "
-		// if we find "refer" line, we do another whois query
-		matched, _ := regexp.MatchString(`^refer:\s+`, lines[i])
+		// if we find "refer" lines, we do another whois query
+		matched, _ := regexp.MatchString(`(?i)(whois\sserver|^refer|^whois):\s+`, lines[i])
 		if matched {
-			re := regexp.MustCompile(`^refer:\s+(.*)`)
-			whoisserver := re.FindStringSubmatch(lines[i])[1]
-			refer_lines := WhoisQuery(domain, whoisserver) 
-			lines = append(lines, refer_lines...)
-			//fmt.Println(domain)
+			re := regexp.MustCompile(`(?i)(whois\sserver|^refer|^whois):\s+([A-Za-z.-]+)`)
+			submatches := re.FindStringSubmatch(lines[i])
+			if (len(submatches) > 0) { // sometimes the whois server field can be empty
+				whoisserver := submatches[len(submatches) - 1]
+				_, contains := QueriedServers[whoisserver] // avoid infinite loops and repeating queries
+				if !contains {
+					referencecomment := "## Found reference to " + whoisserver
+					lines = append(lines, referencecomment)
+					refer_lines := WhoisQuery(domain, whoisserver) 
+					lines = append(lines, refer_lines...)
+				}
+			}
 		}
 	}
 	return lines
-
 }
